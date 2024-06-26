2340d0378036c6356b18a46b3bb75b6cb89b68a4
Muhadils <105841105721@student.unismuh.ac.id>
2024-06-26 05:59:02 -0700

    costum  Komponen

--- App.js
+++ App.js
@@ -1,81 +1,37 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
+const ButtonCostum = ({text,color}) => {
+  return (
+    <View style ={{
+      backgroundColor : color,
+      width : 250,
+      height : 100,
+      borderRadius : 10,
+      justifyContent : 'center',
+      marginBottom :10
+    }}>
+      <Text style ={{
+        textAlign : 'center',
+        color : 'white',
+        fontSize : 30,
+        fontWeight : 'bold',
+      }}>
+        {text}
+      </Text>
+    </View>
+  )
+}
 const App = () => {
   return (
-//     <View style={styles.container}>
-//       <View style={[styles.box, styles.box1]}>
-//         <Text style={styles.text}>Red Section</Text>
-//       </View>
-//       <View style={[styles.box, styles.box2]}>
-//         <Text style={styles.text}>Green Section</Text>
-//       </View>
-//       <View style={[styles.box, styles.box3]}>
-//         <Text style={styles.text}>Blue Section</Text>
-//       </View>
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     padding: 10,
-//     backgroundColor: "#f0f0f0",
-//   },
-//   box: {
-//     flex: 1,
-//     margin: 5,
-//     borderRadius: 10,
-//     justifyContent: "center",
-//     alignItems: "center",
-//   },
-//   box1: {
-//     backgroundColor: "red",
-//   },
-//   box2: {
-//     backgroundColor: "green",
-//   },
-//   box3: {
-//     backgroundColor: "blue",
-//   },
-//   text: {
-//     color: "white",
-//     fontSize: 18,
-//     fontWeight: "bold",
-//   },
-
     <View style ={{
-      marginTop: 400
+      flex : 1,
+      justifyContent : 'center',
+      alignItems : 'center',
     }}>
-      <View style ={{
-        flex : 1, justfyContent: 'center', alignItems: 'flex-center', marginLeft: 70
-      }}>
-        <View style ={{
-          width:100, height:50, backgroundColor:'red', borderRadius:10, justifyContent:'center'
-        }}>
-          <Text style ={{
-            fontSize:20,
-            color:'white',
-            textAlign:'center',
-            marginLeft:'bold'
-          }}>LOGIN</Text>
-      </View>
-      </View>
-      <View style ={{
-        flex:1, justifyContent:'center', alignItems:'flex-end', marginRight:70
-      }}>
-        <View style ={{
-          width:100, height:50, backgroundColor:'blue', borderRadius:'10', justifyContent:'center'
-        }}>
-          <Text style ={{
-            fontSize:20,
-            color:'white',
-            textAlign:'center',
-            fontWeight:'bold'
-          }}>SingUp</Text>
-        </View>
-      </View>
+      <ButtonCostum text = "LOGIN" color = "red" />
+      <ButtonCostum text = "LOGIN" color = "orange" />
+      <ButtonCostum text = "REGIS" color = "purple" />
     </View>
   )
 }
