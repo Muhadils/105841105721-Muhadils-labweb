8db91bb0bacd61670e4c0283abe4101d0262651a
Muhadils <139139811+Muhadils@users.noreply.github.com>
2024-07-25 16:42:28 +0000

    Tugas Navigation & Taskbar

---
+++ bag.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const bag = () => {
+  return (
+    <View>
+      <Text>Ini adalah halaman Bag</Text>
+    </View>
+  )
+}
+
+export default bag
+
+const styles = StyleSheet.create({})
---
+++ shop.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const shop = () => {
+  return (
+    <View>
+      <Text>ini adalah halaman shop</Text>
+    </View>
+  )
+}
+
+export default shop
+
+const styles = StyleSheet.create({})
