1cb62d7189811d229d61808e3a6416fd1ec92896
Muhadils <139139811+Muhadils@users.noreply.github.com>
2024-08-18 16:58:21 +0000

    katalog1 dan 2

--- App.js
+++ App.js
@@ -1,122 +1,120 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, Dimensions, Image, TouchableOpacity, ScrollView } from 'react-native';
-import ImageSlider from 'react-native-image-slider';
+import * as React from 'react';
+import { View, Text, Button, Image, TouchableOpacity, TouchableOpacityBase } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginPage from './loginPage';
+import HomePage from './HomePage';
+import ShopPage from './shop';
+import BagPage from './bag';
+import FavoritePage from './favorit';
+import ProfilePage from './profile';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeAktif from './assets/home-activated.png';
+import HomeInaktif from './assets/home-inactive.png';
+import ShopAktif from './assets/shop-activated.png';
+import ShopInaktif from './assets/shop-inactive.png';
+import BagAktif from './assets/bag-activated.png';
+import BagInaktif from './assets/bag-inactive.png';
+import FavoriteAktif from './assets/favorites-activated.png';
+import FavoriteInaktif from './assets/favorites-inactive.png';
+import ProfileAktif from './assets/profil-activated.png';
+import ProfileInaktif from './assets/profil-inactive.png';
 
-const App = () => {
-  const [position, setPosition] = useState(0);
-
-  const images = [
-    require('./assets/sepatu/sepatu1.jpg'),
-    require('./assets/sepatu/sepatu2.jpg'),
-    require('./assets/sepatu/sepatu3.jpg'),
-    require('./assets/sepatu/sepatu4.jpg'),
-  ];
-
-  const handlePositionChanged = (position) => {
-    setPosition(position);
-  };
-
+const Tab = createBottomTabNavigator();
+function MyTabs() {
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <ImageSlider
-        images={images}
-        autoPlayWithInterval={3000}
-        onPositionChanged={handlePositionChanged}
-        customButtons={(position) => (
-          <View style={styles.pagination}>
-            {images.map((image, index) => (
-              <Text key={index} style={position === index ? styles.activeDot : styles.dot}>
-                •
-              </Text>
-            ))}
-          </View>
-        )}
-        customSlide={({ index, item, style, width }) => (
-          <View key={index} style={[style, styles.customSlide, { width }]}>
-            <Image source={item} style={styles.customImage} />
-          </View>
-        )}
-        onPress={({ index }) => alert(`Pressed image ${index + 1}`)}
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={HomePage}
+        options={{
+          headerShown: false, tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused ? HomeAktif : HomeInaktif}
+              style={{ width: 40, height: 40 }}
+            />
+          ),
+        }}
       />
-      <View style={styles.productDetails}>
-        <Text style={styles.productTitle}>Sepatu Olahraga</Text>
-        <Text style={styles.productPrice}>Rp 750.000</Text>
-        <Text style={styles.productDescription}>
-          Sepatu olahraga berkualitas tinggi, dirancang untuk kenyamanan dan performa maksimal. Cocok untuk berbagai aktivitas olahraga.
-        </Text>
-        <TouchableOpacity style={styles.buyButton}>
-          <Text style={styles.buyButtonText}>Beli Sekarang</Text>
-        </TouchableOpacity>
-      </View>
-    </ScrollView>
+      <Tab.Screen
+        name="Shop"
+        component={ShopPage}
+        options={{
+          headerShown: false, tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused ? ShopAktif : ShopInaktif}
+              style={{ width: 40, height: 40 }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Bag"
+        component={BagPage}
+        options={{
+          headerShown: false, tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused ? BagAktif : BagInaktif}
+              style={{ width: 40, height: 40 }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Favorite"
+        component={FavoritePage}
+        options={{
+          headerShown: false, tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused ? FavoriteAktif : FavoriteInaktif}
+              style={{ width: 40, height: 40 }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfilePage}
+        options={{
+          headerShown: false, tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused ? ProfileAktif : ProfileInaktif}
+              style={{ width: 40, height: 40 }}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    backgroundColor: '#fff',
-    padding: 20,
-  },
-  pagination: {
-    position: 'absolute',
-    bottom: -20,
-    left: 0,
-    right: 0,
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'row',
-  },
-  dot: {
-    fontSize: 30,
-    color: '#888',
-    margin: 3,
-  },
-  activeDot: {
-    fontSize: 30,
-    color: '#fff',
-    margin: 3,
-  },
-  customSlide: {
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
-  customImage: {
-    width: Dimensions.get('window').width - 40,
-    height: 300,
-    resizeMode: 'cover',
-  },
-  productDetails: {
-    marginTop: 20,
-    alignItems: 'center',
-  },
-  productTitle: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  productPrice: {
-    fontSize: 20,
-    color: '#e91e63',
-    marginBottom: 20,
-  },
-  productDescription: {
-    fontSize: 16,
-    color: '#333',
-    textAlign: 'center',
-    marginBottom: 20,
-    paddingHorizontal: 10,
-  },
-  buyButton: {
-    backgroundColor: '#e91e63',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 5,
-  },
-  buyButtonText: {
-    color: '#fff',
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-});
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Login"
+        onPress={() => navigation.navigate('Login')}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={MyTabs} options={{headerShown: false}} />
+        {/* <Stack.Screen name="Login" component={LoginPage} options={{headerShown: false}} /> */}
+        <Stack.Screen name="Shop" component={ShopPage} options={{headerShown: false}} />
+        <Stack.Screen name="Bag" component={BagPage} options={{headerShown: false}} />
+        <Stack.Screen name="Favorite" component={FavoritePage} options={{headerShown: false}} />
+        <Stack.Screen name="Profile" component={ProfilePage} options={{headerShown: false}} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
